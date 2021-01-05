2b44e77c26c88c80ab7040c3f5c21c6ef58d751a
SalmaTADLAOUI <76519142+SalmaTADLAOUI@users.noreply.github.com>
2021-01-05 21:03:03 +0100

    Update ex04.c

--- ex04.c
+++ ex04.c
@@ -1,68 +1,88 @@
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 #define N 10
-void get_input(float A[][N], float *B, int *n)
-{                                                                                               
-	printf("enter the matrix dimension : ");
-	scanf("%d",n);                
-	for(int i=0; i<*n; i++)  
-	{ 
-		for(int j=0; j<*n; j++) 
-		{       
-			printf("A[%d][%d] =",i,j);  
-			scanf("%f", &A[i][j]);   
-		}         
-	} 
-	for(int i=0; i<*n; i++)    
-	{   
-		printf("B[%d]=",i);   
-		scanf("%f", &B[i]);  
-	}     
-}                                
+
+float cholesky(float C[N][N], float A[N][N],int n)
+{
+        for(int j = 0; j < n; j++){
+                for(int i = j; i < n; i++){
+                        if(i == j)
+                        {
+                                float s = 0;
+                                for(int k = 0; k < j; k++){
+                                        s += C[i][k]*C[i][k];
+                                }
+                                C[i][j] = sqrt(A[i][j]-s);
+                        }
+                        else
+                        {
+                                float s2 = 0;
+                                for(int k=0; k < j; k++){
+                                        s2 += C[i][k]*C[j][k];
+                                }
+                                C[i][j]=(A[i][j]-s2)/C[j][j];
+                        }
+                }
+        }
+        return C[N][N];
+}
+
 int main()
 {
-	float A[N][N];
-	float B[N];
-	float X[N],C[N][N];
-	float s,u,n;
-	C[0][0]=sqrt(A[0][0]);
-	for(int i=1; i<n; i++)
-	{
-		C[i][0]=A[i][0];
-	}     
-	for(int j=1; j<n; j++)
-	{
-		for(int i=0; i<j; i++)
-		{
-			C[i][j]=0;
-		}
-		for(int k=0; k<j ; k++)
-		{
-			s+=C[j][k]*C[j][k];
-		}
-		C[j][j]=sqrt(A[j][j]-s);
-		for(int i=j+1; i<n; i++)
-		{
-			for(int k=0; k<j; k++)
-			{	
-				u+=C[i][k]*C[j][k];
-			}	
-			C[i][j]=(A[i][j]-u)/C[j][j];
-		}
-		for(int i=0; i<n; i++)
-		{
-			for(int j=0; j<n ; j++)
-			{
-				printf("%lf\n",C[i][j]);
-			}	
-		}
+        int n;
+        float A[N][N], B[N], Y[N], X[N], C[N][N];
+        printf("enter the dimension of the matrix ");
+        scanf("%d", &n);
+        for(int i = 0; i < n; i++)
+                for(int j = 0; j < n ; j++){
+                        printf("A[%d][%d]= ", i, j);
+                        scanf("%f", &A[i][j]);
+                }
 
+        for(int i=0; i < n; i++){
+                printf("B[%d]=", i);
+                scanf("%f", &B[i]);
+        }
 
-	}
+        for(int i = 0; i < n; i++)
+        {
+                for(int j = 0; j < n; j++){
+                        C[i][j]=0;
+                }
+        }
+        C[N][N]=cholesky(C, A, n);
+        printf("Here is the C matrix \n");
+        for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++)
+                {
+                        printf("C[%d][%d] = %f\t",i, j, C[i][j]);
+                }
+                printf("\n");
+        }
+        Y[0] = B[0] / C[0][0];
+        for(int i = 1; i < n; i++){
+                float s=0;
+                for(int j=0; j <= i-1; j++){
+                        s += C[i][j] * Y[j];
+                }
+                Y[i]=(B[i]-s)/C[i][i];
+        }
+        for(int i = 0; i < n ; i++)
+        {
+                printf("Y[%d]=%f\t",i, Y[i]);
+        }
+        X[n-1] = Y[n-1]/C[n-1][n-1];
 
-
-
-
-
-}	
+        for(int i = n-2; i >= 0; i--){
+                float s2=0;
+                for(int j = i+1; j < n; j++){
+                        s2 += C[j][i] * X[j];
+                }
+                X[i] = (Y[i]-s2)/C[i][i];
+        }
+        printf("\n the final solution is :\n");
+        for(int i=0; i < n; i++)
+                printf("X[%d]=%f\t",i, X[i]);
+        return (0);
+}
